9b25dc8ddef4475b0ebc986ca8b1e467d7554ce3
Viper <viperxx100@gmail.com>
2023-05-19 02:51:34 +0300

    #

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express')
-// import cors from 'cors'
+// const express = require('express')
+// // import cors from 'cors'
 
-const host = 'http://localhost'
-const port = 6660
+// const host = 'http://localhost'
+// const port = 6660
 
-const app = express()
-app.use(express.json())
+// const app = express()
+// app.use(express.json())
 
-// 
-app.get('/', (req, res) => {
-    res.status(200).json({'hello': 'world'})
-})
+// // 
+// app.get('/', (req, res) => {
+//     res.status(200).json({'hello': 'world'})
+// })
 
-app.listen(port, () => console.log(`Server listening on ${host}:${port}`));
+// app.listen(port, () => console.log(`Server listening on ${host}:${port}`));
