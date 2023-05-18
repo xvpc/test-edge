51a60bb403379a6557850a068d43763a2df1b6ad
Viper <viperxx100@gmail.com>
2023-05-19 01:05:23 +0300

    #

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // experimental: { runtime: 'edge'}
+  experimental: { runtime: 'edge'}
 }
 
 module.exports = nextConfig
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,10 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-export const config = { runtime: 'edge' }
 
 type Data = {
   name: string
 }
+export const config = { runtime: 'edge' }
 
 export default  function handler(req: NextApiRequest, res: NextApiResponse<Data>){
 
