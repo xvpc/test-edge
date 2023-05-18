8a9424c0ef47b9f0bae506999d08315f9c135bbd
Viper <viperxx100@gmail.com>
2023-05-18 23:57:11 +0300

    #

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios'
 import type { NextApiRequest, NextApiResponse } from 'next'
+export const config = { runtime: 'edge' }
 
 type Data = {
   name: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+
+  const fetchData = await axios.get('http://localhost:3001/api/anilist/trending')
+
+  res.status(200).json(fetchData?.data as any)
 }
